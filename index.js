d62670492a3dcb29ac7d95874f720dd0418c4238
Tulio Faria <tuliofaria@gmail.com>
2021-05-13 22:20:39 -0300

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({
+    status: 'running',
+  })
+})
+app.listen(3000, (err) => {
+  if (!err) {
+    console.log(err)
+  } else {
+    console.log('server running...')
+  }
+})
---
+++ notes.md
@@ -0,0 +1,8 @@
+# Processo: idealmente feito no começo
+
+Bootstrap
+
+- Monorepo: todos os projetos no mesmo repo
+  - javascript: npm install
+  - python: pip install
+- Multi repo: cada projeto no seu repo
